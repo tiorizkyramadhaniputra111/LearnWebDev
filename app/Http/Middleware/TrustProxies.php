abc01a923a868ee39ed79c6eb54b6b488d8387e8
tiorizkyramadhaniputra111 <tiorizkyramadhaniputra@gmail.com>
2026-01-31 17:43:11 +0700

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,9 +9,5 @@ class TrustProxies extends Middleware
 {
     protected $proxies = '*';
 
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO;
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
